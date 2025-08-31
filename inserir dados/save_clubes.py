5bf5f90bfd65ef061203d643f1f196dbfcff9a0b
Josef <jcaique10@gmail.com>
2025-08-31 01:54:10 -0300

    feat: gathering data from clubes api

--- inserir dados/save_clubes.py
+++ inserir dados/save_clubes.py
@@ -1,14 +1,24 @@
 import requests
 import json
-from config.db import getConnection
+#from config.db import getConnection
 
 
+#conn = getConnection()
+#cursor = conn.cursor()
 
 
+query = "INSERT INTO clubes VALUES (%s, %s, %s, %s, %s)"
 
-conn = getConnection()
-cursor = conn.cursor()
-conn.commit()
-cursor.close()
-conn.close()
+data = requests.get("https://api.cartola.globo.com/clubes")
+json = data.json()
+for k in json:
+    value = json[k]
+    name = value["nome"]
+    abr = value["abreviacao"]
+    slug = value["slug"]
+    fantasy_name = value["nome_fantasia"]
+    id = str(value["id"])
 
+    data = (id, name, abr, slug, fantasy_name)
+    print(data)
+    
